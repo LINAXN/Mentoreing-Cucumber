f42c74658e4fa25aa997651969d26bdd1b37b5d1
Lina Khaled <linaxn4@gmail.com>
2025-10-07 15:53:54 +0300

    Add parameterized tests for form functionality; implement FormPage and step definitions

---
+++ src/test/java/utilities/Hooks.java
@@ -0,0 +1,13 @@
+package utilities;
+
+import io.cucumber.java.After;
+import utilities.Driver;
+
+public class Hooks {
+
+    @After("@DeleteChars")//import io.cucumber.java.After;
+    public void tearDown() {
+        Driver.closeDriver();
+    }
+
+}
